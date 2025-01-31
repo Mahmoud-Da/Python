e0d5e43a114c28e7e7058b01bfacb8051b65fb6e
Mahmoud-Da <mdabbagh6@gmail.com>
2025-02-01 00:15:41 +0900

    9- Type Conversion - #4

---
+++ Complete-Python-Mastery/2-Primitive-Types/app9.py
@@ -0,0 +1,23 @@
+# x = input("x: ")
+# print(type(x))
+# # y = x + 1
+
+x = input("x: ")
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
+
+int(x)
+float(x)
+bool(x)
+str(x)
+type(x)
+
+
+# Falsy
+""
+0
+None
+
+print(bool(0))
+print(bool("test"))
+print(bool("False"))

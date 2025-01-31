097bed55dc34ff3373e54e639434c093d8dbc09a
Mahmoud-Da <mdabbagh6@gmail.com>
2025-01-31 23:13:27 +0900

    7- Numbers

---
+++ Complete-Python-Mastery/2-Primitive-Types/app7.py
@@ -0,0 +1,20 @@
+# integer
+x = 1
+# float
+x = 1.1
+# complex
+x = 1 + 2j
+
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+x = 10
+x = x + 3
+x += 3
+print(x)

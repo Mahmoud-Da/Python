307334a9cd358d2df7eb166e2c8e34d0ad903472
Mahmoud-Da <mdabbagh6@gmail.com>
2025-02-01 17:23:06 +0900

    2- Conditional Statements

---
+++ Complete-Python-Mastery/3-Control-Flow/app2.py
@@ -0,0 +1,39 @@
+temperature = 37
+if temperature > 30:
+    print("it's worm")
+    print("Drink water")
+print("Done")
+
+# it's worm
+# Drink water
+# Done
+
+temperature = 15
+if temperature > 30:
+    print("it's worm")
+    print("Drink water")
+print("Done")
+# Done
+
+temperature = 21
+if temperature > 30:
+    print("it's worm")
+    print("Drink water")
+elif temperature > 20:
+    print("it's nice")
+print("Done")
+# it's nice
+# Done
+
+
+temperature = 15
+if temperature > 30:
+    print("it's worm")
+    print("Drink water")
+elif temperature > 20:
+    print("it's nice")
+else:
+    print("it's cold")
+print("Done")
+# it's cold
+# Done

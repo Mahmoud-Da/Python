66e8ac22760a196a52c7b917d109e68f69ade8b8
Mahmoud-Da <mdabbagh6@gmail.com>
2025-02-01 20:27:41 +0900

    14- Exercise - #5

---
+++ Complete-Python-Mastery/3-Control-Flow/app14.py
@@ -0,0 +1,27 @@
+count = 0
+for i in range(1, 10):
+    if i % 2 == 0:
+        count += 1
+        print(i)
+print(f"We have {count} even numbers")
+
+# 2
+# 4
+# 6
+# 8
+# We have 4 even numbers
+
+count = 0
+i = 1
+while i < 10:
+    if i % 2 == 0:
+        count += 1
+        print(i)
+    i += 1
+print(f"We have {count} even numbers")
+
+# 2
+# 4
+# 6
+# 8
+# We have 4 even numbers

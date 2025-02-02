1a24bc81bb87b7d1d405b7e9bb71bcad99f3c8d4
Mahmoud-Da <mdabbagh6@gmail.com>
2025-02-02 20:43:20 +0900

    5- Default Arguments - #6

---
+++ Complete-Python-Mastery/4-Functions/app5.py
@@ -0,0 +1,13 @@
+def increment(number, by=1):
+    return number + by
+
+
+print(increment(2))
+# 3
+
+print(increment(2, 2))
+# 4
+
+
+# def increment(number, by=1, another):
+#     return number + by

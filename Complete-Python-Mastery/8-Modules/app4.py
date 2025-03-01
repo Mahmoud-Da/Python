48ae06cc0f34c0d1c224523530aaabf1219ed157
Mahmoud-Da <mdabbagh6@gmail.com>
2025-03-01 22:44:00 +0900

    4- Packages - #10

---
+++ Complete-Python-Mastery/8-Modules/app4.py
@@ -0,0 +1,9 @@
+# import sales
+# import ecommerce.sales
+# from ecommerce.sales import calc_shipping
+# from ecommerce.sales import calc_shipping, calc_tax
+from ecommerce import sales
+
+# ecommerce.sales.calc_shipping()
+# calc_shipping()
+sales.calc_shipping()

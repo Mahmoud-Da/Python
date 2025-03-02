d4dda3468a375aac120b14ac20ecf6b2faeeddd3
Mahmoud-Da <mdabbagh6@gmail.com>
2025-03-02 21:07:09 +0900

    2- Working With Paths - #11

--- Complete-Python-Mastery/9-Python-Standard-Library/app2.py
+++ Complete-Python-Mastery/9-Python-Standard-Library/app2.py
@@ -1,19 +1,43 @@
 from pathlib import Path
 
+Path("C:\\Program Files\\Microsoft")
 Path(r"C:\Program Files\Microsoft")
 Path("/usr/local/bin")
 Path()
-Path("ecommerce/__init__py")
-Path() / Path("'ecommerce")
+Path("ecommerce/__init__.py")
+Path() / Path("ecommerce")
 Path() / "ecommerce"
-Path() / "ecommerce" / "__init__py"
+Path() / "ecommerce" / "__init__.py"
 Path.home()
 
 
-path = Path("ecommerce/__init__py")
+path = Path("ecommerce/__init__.py")
 
-# python3 pathlib
 
 path.exists()
 path.is_file()
 path.is_dir()
+
+print(path.name)
+# __init__.py
+
+print(path.stem)
+# __init__
+
+print(path.suffix)
+# .py
+
+print(path.parent)
+# ecommerce
+
+path = path.with_name("file.txt")
+print(path)
+# ecommerce/file.txt
+
+print(path.absolute())
+# /Python/ecommerce/file.txt
+
+
+path = path.with_suffix(".py")
+print(path)
+# ecommerce/file.py

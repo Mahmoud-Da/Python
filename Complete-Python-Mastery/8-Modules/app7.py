a9b1d1bf9bd1a1a9a3d8b0b694e873b94da7f062
Mahmoud-Da <mdabbagh6@gmail.com>
2025-03-02 13:52:42 +0900

    7- The dir Function - #10

---
+++ Complete-Python-Mastery/8-Modules/app7.py
@@ -0,0 +1,13 @@
+from ecommerce.shopping import sales
+
+
+print(dir(sales))
+# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'calc_shipping', 'calc_tax', 'contact']
+
+
+print(sales.__name__)
+# ecommerce.shopping.sales
+print(sales.__package__)
+# ecommerce.shopping
+print(sales.__file__)
+# /Complete-Python-Mastery/8-Modules/ecommerce/shopping/sales.py
